1f611ebe40483c813fabf5bf8e20233685c0dfbf
LeoGBobic <130657687+LeoGBobic@users.noreply.github.com>
2023-07-30 00:53:49 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,31 +1,3 @@
-//var i = 0; 			// Start Point
-//var images = [];	// Images Array
-//var time = 4000;	// Time Between Switch
-	 
-// Image List
-//images[0] = "Slike/dec1.jpg";
-//images[1] = "Slike/dec2.jpg";
-//images[2] = "Slike/dec3.jpg";
-//images[3] = "Slike/dec4.jpg";
-
-// Change Image
-//function changeImg(){
-//	document.slide.src = images[i];
-
-	// Check If Index Is Under Max
-//	if(i < images.length - 1){
-	  // Add 1 to Index
-//	  i++; 
-//	} else { 
-		// Reset Back To O
-//		i = 0;
-//	}
-
-	// Run function every x seconds
-//	setTimeout("changeImg()", time);
-//}
-
-//Svatov
 // Run function when page loads
 window.onload=changeImg;
 
