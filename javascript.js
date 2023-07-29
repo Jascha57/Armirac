cf89ed3470969bf324c0987bf7e9666b06104b8f
LeoGBobic <130657687+LeoGBobic@users.noreply.github.com>
2023-07-29 21:55:04 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -25,11 +25,11 @@
 //	setTimeout("changeImg()", time);
 //}
 
-//Svatovi
+//Svatov
 // Run function when page loads
 window.onload=changeImg;
 
-                     //const imgBx = document.querySelector('.imgBx');
+ const imgBx = document.querySelector('.imgBx');
 const slides = imgBx.getElementsByTagName('img');
 var i = 0;
 var interval = 5000;
