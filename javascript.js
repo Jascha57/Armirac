24014940a9f6912c4f99b84e6d6f99285e8f141b
LeoGBobic <130657687+LeoGBobic@users.noreply.github.com>
2023-07-29 21:23:53 +0200

    Update javascript.js
    Svatovi

--- javascript.js
+++ javascript.js
@@ -26,4 +26,17 @@ function changeImg(){
 }
 
 // Run function when page loads
-window.onload=changeImg;
+//window.onload=changeImg;
+
+                     //const imgBx = document.querySelector('.imgBx');
+//const slides = imgBx.getElementsByTagName('img');
+//var i = 0;
+//var interval = 5000;
+
+//setInterval(function(){
+//    slides[i].classList.remove('active');
+//    slides[i].classList.add('hide');
+//    i = (i + 1) % slides.length;
+//    slides[i].classList.remove('hide');
+//    slides[i].classList.add('active');
+//}, interval);
