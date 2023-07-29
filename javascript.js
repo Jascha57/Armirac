fa77214a90e7e538a183e85c17e199724406527a
LeoGBobic <130657687+LeoGBobic@users.noreply.github.com>
2023-07-29 21:33:06 +0200

    Update javascript.js
    TEST SVATOVI

--- javascript.js
+++ javascript.js
@@ -1,42 +1,43 @@
-var i = 0; 			// Start Point
-var images = [];	// Images Array
-var time = 4000;	// Time Between Switch
+//var i = 0; 			// Start Point
+//var images = [];	// Images Array
+//var time = 4000;	// Time Between Switch
 	 
 // Image List
-images[0] = "Slike/dec1.jpg";
-images[1] = "Slike/dec2.jpg";
-images[2] = "Slike/dec3.jpg";
-images[3] = "Slike/dec4.jpg";
+//images[0] = "Slike/dec1.jpg";
+//images[1] = "Slike/dec2.jpg";
+//images[2] = "Slike/dec3.jpg";
+//images[3] = "Slike/dec4.jpg";
 
 // Change Image
-function changeImg(){
-	document.slide.src = images[i];
+//function changeImg(){
+//	document.slide.src = images[i];
 
 	// Check If Index Is Under Max
-	if(i < images.length - 1){
+//	if(i < images.length - 1){
 	  // Add 1 to Index
-	  i++; 
-	} else { 
+//	  i++; 
+//	} else { 
 		// Reset Back To O
-		i = 0;
-	}
+//		i = 0;
+//	}
 
 	// Run function every x seconds
-	setTimeout("changeImg()", time);
-}
+//	setTimeout("changeImg()", time);
+//}
 
+//Svatovi
 // Run function when page loads
-//window.onload=changeImg;
+window.onload=changeImg;
 
                      //const imgBx = document.querySelector('.imgBx');
-//const slides = imgBx.getElementsByTagName('img');
-//var i = 0;
-//var interval = 5000;
+const slides = imgBx.getElementsByTagName('img');
+var i = 0;
+var interval = 5000;
 
-//setInterval(function(){
-//    slides[i].classList.remove('active');
-//    slides[i].classList.add('hide');
-//    i = (i + 1) % slides.length;
-//    slides[i].classList.remove('hide');
-//    slides[i].classList.add('active');
-//}, interval);
+setInterval(function(){
+    slides[i].classList.remove('active');
+    slides[i].classList.add('hide');
+    i = (i + 1) % slides.length;
+    slides[i].classList.remove('hide');
+    slides[i].classList.add('active');
+}, interval);
